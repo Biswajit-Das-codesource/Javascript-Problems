6ab8612ce6f6eb527e80ff099f19f67b21de4026
biswajit das <biswajit9348das@gmail.com>
2024-11-21 08:07:28 +0530

    set

---
+++ index.js
@@ -0,0 +1,16 @@
+// Find Duplicate word in sentence 
+
+let sentence= "hiii my my name name is biswajit das".split(" ")
+
+wordcount={}
+duplicate=[]
+for(let i of sentence){
+    wordcount[i]=(wordcount[i] || 0)+1
+}
+for(let j in wordcount){
+    if((wordcount[j])>1){
+        duplicate.push(j)
+    }
+}
+console.log(duplicate)
+
