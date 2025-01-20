65c32411958d4098be80eb3f013d4ef66da6d08c
biswajit das <biswajit9348das@gmail.com>
2025-01-20 11:04:38 +0530

    ok

---
+++ MaxString.js
@@ -0,0 +1,20 @@
+function calculate(obj){
+
+    // console.log(obj)
+
+    let arr=[]
+    for(let i of obj){
+        for(let j of obj){
+            arr.push(i+j)
+        }
+    }
+    console.log(typeof arr)
+    return arr.reduce((largest, current) => 
+        current.length > largest.length ? current : largest, 
+        ""
+      );
+
+}
+
+obj = ["hello", "aja","apple","banana","pie"]
+console.log(calculate(obj))
